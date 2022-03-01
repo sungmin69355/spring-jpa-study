08976c661048983f689e185cf9ab7e45f5c8d036
sungmin69355 <sungmin69355@naver.com>
2022-03-01 22:05:35 +0900

    jpa h2 환경설정 테스트

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em; //스프링부트가 만들어줌
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId(); //커맨드랑 코드랑 분리하기위해 아이디만 반환
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+}
