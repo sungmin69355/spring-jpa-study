095b3d092dc66106d6ea68c492542857f2cc2358
sungmin69355 <sungmin69355@naver.com>
2022-03-06 15:40:17 +0900

    fix:회원 레파지토리 설계

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository //빈등록
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em; //엔티티매니저만들어서 위임한다.
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class,id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m",Member.class) //쿼리,반환형식
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name",Member.class)
+                .setParameter("name",name)
+                .getResultList();
+    }
+}
